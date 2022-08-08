a16324b574c4e1708ffc700f4d3d9da5aef574c8
enkidur <lastagnel40@gmail.com>
2022-08-08 11:45:52 +0900

    Initial commit

---
+++ src/main/java/com/project/uandmeet/UandmeetApplication.java
@@ -0,0 +1,13 @@
+package com.project.uandmeet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UandmeetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UandmeetApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/project/uandmeet/UandmeetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.uandmeet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UandmeetApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
