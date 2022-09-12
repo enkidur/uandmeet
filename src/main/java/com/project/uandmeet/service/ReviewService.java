39c8c4abed40cee9863567888b43371564fe38cc
LAPTOP-4RG8174M\User <wjdgns5488@naver.com>
2022-09-12 17:27:00 +0900

    [리뷰] - 리뷰 작성 수정

--- src/main/java/com/project/uandmeet/service/ReviewService.java
+++ src/main/java/com/project/uandmeet/service/ReviewService.java
@@ -1,10 +1,13 @@
 package com.project.uandmeet.service;
 
 import com.project.uandmeet.dto.*;
+import com.project.uandmeet.exception.CustomException;
+import com.project.uandmeet.exception.ErrorCode;
 import com.project.uandmeet.model.Board;
 import com.project.uandmeet.model.Member;
 import com.project.uandmeet.model.Review;
 import com.project.uandmeet.repository.BoardRepository;
+import com.project.uandmeet.repository.EntryRepository;
 import com.project.uandmeet.repository.ReviewRepository;
 import com.project.uandmeet.security.UserDetailsImpl;
 import lombok.RequiredArgsConstructor;
@@ -17,11 +20,12 @@ import org.springframework.stereotype.Service;
 public class ReviewService {
     private final ReviewRepository reviewRepository;
     private final BoardRepository boardRepository;
+    private final EntryRepository entryRepository;
 
     public ReviewResponseDto review(UserDetailsImpl userDetails, BoardIdRequestDto requestDto) {
         String nickname = userDetails.getMember().getNickname();
         Board board = boardRepository.findById(requestDto.getBoardId()).orElseThrow(
-                ()-> new RuntimeException("찾을 수 없는 게시글입니다.")
+                () -> new RuntimeException("찾을 수 없는 게시글입니다.")
         );
         Member otherMember = board.getMember();
         ReviewResponseDto reviewResponseDto = new ReviewResponseDto(nickname, otherMember);
@@ -31,13 +35,14 @@ public class ReviewService {
 
     public ReviewDto createReview(UserDetailsImpl userDetails, ReviewRequestDto requestDto) {
         Member from = userDetails.getMember();
-//        Member to = memberRepostiory.findById(requestDto.getToId()).orElseThrow(
-//                ()->new RuntimeException("존재하지 않는 사용자입니다.")
-//        );
         Board board = boardRepository.findById(requestDto.getBoardId()).orElseThrow(
                 () -> new RuntimeException("찾을 수 없는 게시글입니다.")
         );
-        Review review = new Review(board, from.getId(), board.getMember(), requestDto.getNum(), requestDto.getScore() ,requestDto.getReview());
+        if (!entryRepository.existsByMemberAndAndBoard(from, board)) {
+            throw new CustomException(ErrorCode.INVALID_AUTHORITY);
+        }
+
+        Review review = new Review(board, from.getId(), board.getMember(), requestDto.getNum(), requestDto.getScore(), requestDto.getReview());
         reviewRepository.save(review);
         return new ReviewDto(board.getId(),
                 from.getId(),
