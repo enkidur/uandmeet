404158497e797aed455a2a924e7cb3c482bcc68b
Seonghun Lee <aba7500@naver.com>
2022-09-13 16:25:34 +0900

    [ 검색 처리 ] information Type 로직 변경

--- src/main/java/com/project/uandmeet/dto/SearchResponseDto.java
+++ src/main/java/com/project/uandmeet/dto/SearchResponseDto.java
@@ -16,6 +16,7 @@ public class SearchResponseDto {
     private String title;
     private String content;
     private String boardimage;
+
     private LocalDateTime endDateAt;
     private String city;
     private String gu;
@@ -37,6 +38,7 @@ public class SearchResponseDto {
     //위도
     private double lng;
 
+    // 매칭
     public SearchResponseDto(Board board){
         this.city = board.getCity().getCtpKorNmAbbreviation();
         this.gu = board.getGu().getSigKorNm();
@@ -54,7 +56,24 @@ public class SearchResponseDto {
         this.profile=board.getMember().getProfile();
         this.username=board.getMember().getUsername();
         this.boardType=board.getBoardType();
+    }
 
+    //정보공유
+    public SearchResponseDto(Board board,String bt){
+        this.id = board.getId();
+        this.createdAt = board.getCreatedAt();
+        this.category = board.getCategory().getCategory();
+        this.title = board.getTitle();
+        this.boardimage = board.getBoardimage();
+        this.likeCount = board.getLikeCount();
+        this.commentCount = board.getCommentCount();
+        this.maxEntry = board.getMaxEntry();
+        this.currentEntry = board.getCurrentEntry();
+        this.content=board.getContent();
+        this.nickname=board.getMember().getNickname();
+        this.profile=board.getMember().getProfile();
+        this.username=board.getMember().getUsername();
+        this.boardType=bt;
     }
 
 }
